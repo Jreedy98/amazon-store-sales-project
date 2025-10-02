6f81953fe4e19cd740ce014925d30340fbf613b6
Jreedy98 <jakesjob98@gmail.com>
2025-10-02 11:33:07 +0100

    Create creating_test_table.sql

---
+++ sql_queries/creating_test_table.sql
@@ -0,0 +1,18 @@
+ALTER TABLE sales_data
+	ADD product_id TEXT,
+	ADD product_name TEXT,
+	ADD category TEXT,
+	ADD discounted_price TEXT,
+	ADD actual_price TEXT,
+	ADD discount_percentage TEXT,
+	ADD rating TEXT,
+	ADD rating_count TEXT,
+	ADD about_product TEXT,
+	ADD user_id TEXT,
+	ADD user_name TEXT,
+	ADD review_id TEXT,
+	ADD review_title TEXT,
+	ADD review_content TEXT,
+	ADD img_link TEXT,
+	ADD product_link TEXT
+;
